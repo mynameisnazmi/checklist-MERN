81039ffc80260f7b3d7f3545ea96629020a6a573
Sazza540 <athbadanir@gmail.com>
2022-12-21 09:54:37 +0700

    commit 2
    responsive

--- src/App.js
+++ src/App.js
@@ -1,46 +1,9 @@
-import logo from "./Asset/logo.png";
+import Login from "./components/Login";
 
 export default function App() {
   return (
     <>
-      <div className="grid grid-cols-2 h-screen">
-        <div className="grid bg-blue-300 h-full w-full place-content-center">
-          <form>
-            <div className="flex flex-col h-full w-full bg-red-200 gap-1 rounded-md">
-              <label className="self-center py-2">Log in</label>
-
-              <input
-                className="mx-2 my-2 px-2 py-2 rounded-md"
-                placeholder="Username"
-                type="text"
-              ></input>
-
-              <input
-                className="mx-2 my-2 px-2 py-2 rounded-md"
-                placeholder="Password"
-                type="password"
-              ></input>
-
-              <button
-                type="button"
-                className=" self-center max-w-fit max-h-fit text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-md text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-              >
-                Sign In
-              </button>
-              {/* <a className="self-center mb-1 -mt-3 px-1 py-2" href="#">
-                Sign Up
-              </a> */}
-            </div>
-          </form>
-        </div>
-
-        <div className="bg-sky-300/50 h-full md:block sm:hidden">
-          <div
-            className="h-full bg-no-repeat bg-contain bg-center mx-5"
-            style={{ backgroundImage: "url(" + logo + ")" }}
-          ></div>
-        </div>
-      </div>
+      <Login></Login>
     </>
   );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,49 +1,48 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-
+import logo from "../Asset/logo.png";
 function Login() {
-  // [Actual Value, SET Value] = useState(Default value)
-  const [NIK, setNIK] = useState(0);
-  const [password, setPassword] = useState("");
-
-  const handleSubmit = (e) => {
-    // arrow function
-    e.preventDefault(); //prevent default behavior
-
-    const datalogin = {
-      //create object
-      nik: NIK,
-      password: password,
-    };
-    console.log(datalogin);
-  };
-
   return (
     <>
-      <ul>
-        <li>
-          <Link to="/Register">Register</Link>
-        </li>
-        <li>
-          <Link to="/Dashbord">Dashbord</Link>
-        </li>
-      </ul>
-      <div className="bg-slate-500 grid place-content-center h-screen">
-        <form className="flex flex-col" onSubmit={handleSubmit}>
-          <label>NIK</label>
-          <input
-            type="number"
-            value={NIK}
-            onChange={(e) => setNIK(e.target.value)}
-          />
-          <label>Password</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <button>Sign In</button>
-        </form>
+      <div className="grid grid-cols-2 h-screen max-sm:grid-cols-1">
+        <div className="grid bg-blue-300 h-full w-full place-content-center max-sm:grid-rows-4">
+          <div
+            className="h-1/2 bg-no-repeat bg-contain bg-center mt-auto"
+            style={{ backgroundImage: "url(" + logo + ")" }}
+          ></div>
+          <form>
+            <div className="flex flex-col h-full w-full bg-red-200 gap-1 rounded-md">
+              <label className="self-center py-2">Log in</label>
+
+              <input
+                className="mx-2 my-2 px-2 py-2 rounded-md"
+                placeholder="Username"
+                type="text"
+              ></input>
+
+              <input
+                className="mx-2 my-2 px-2 py-2 rounded-md"
+                placeholder="Password"
+                type="password"
+              ></input>
+
+              <button
+                type="button"
+                className=" self-center max-w-fit max-h-fit text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-md text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+              >
+                Sign In
+              </button>
+              {/* <a className="self-center mb-1 -mt-3 px-1 py-2" href="#">
+                Sign Up
+              </a> */}
+            </div>
+          </form>
+        </div>
+
+        <div className="bg-sky-300/50 h-full max-sm:hidden">
+          <div
+            className="h-full bg-no-repeat bg-contain bg-center mx-5 max-sm:hidden"
+            style={{ backgroundImage: "url(" + logo + ")" }}
+          ></div>
+        </div>
       </div>
     </>
   );
