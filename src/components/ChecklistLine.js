8fae8c3032e82829aba3183174d6a827b8d08a79
Sazza540 <athbadanir@gmail.com>
2023-01-02 16:46:55 +0700

    222

--- src/components/ChecklistLine.js
+++ src/components/ChecklistLine.js
@@ -5,7 +5,7 @@ function ChecklistCT() {
     <>
       <div className="flex flex-col h-screen bg-slate-100">
         <div className="basis-[16%] flex-col">
-          <SectionHeader></SectionHeader>
+          <SectionHeader />
           <div className="flex flex-col  text-xl  items-center justify-center h-fit m-2 self-center sm:flex-row sm:text-4xl">
             <span>Line-4</span>
             <select className="border-2 mx-10">
@@ -19,46 +19,35 @@ function ChecklistCT() {
             </select>
           </div>
         </div>
-        <div className="basis-[69%] bg-white items-start justify-center w-screen overflow-x-auto ">
-          <table class="table-auto text-center border-2 whitespace-nowrap text-[100%] sm:text-lg md:text-2xl lg:text-base">
-            <thead className="border-b-2 border-collapse w-screen">
+        <div className="relative basis-[69%] bg-white items-start justify-center w-screen overflow-x-auto ">
+          <table className=" table-fixed text-center border-2 whitespace-nowrap text-[100%] sm:text-lg md:text-2xl lg:text-base">
+            <thead className="border-b-2 border-collapse w-auto">
               <tr>
-                <th className="w-[4%] px-2 sm:px-4 md:px-4 lg:px-0">No</th>
-                <th className="w-[14%] px-2 sm:px-4 md:px-4 lg:px-0">Motor</th>
-                <th className="w-[8%] px-4 sm:px-4 md:px-4 lg:px-0">
-                  Stetoskop
+                <th className="sticky left-0 bg-white w-40 px-0 sm:px-4 md:px-4 lg:px-0">
+                  No
                 </th>
-                <th className="w-[8%] px-4  sm:px-4 md:px-4 lg:px-0">Arus R</th>
-                <th className="w-[8%] px-4 sm:px-4 md:px-4 lg:px-0">Arus S</th>
-                <th className="w-[8%] px-4 sm:px-4 md:px-4 lg:px-0">Arus T</th>
-                <th className="w-[8%] px-4 sm:px-4 md:px-4 lg:px-0">
-                  V DE mm/s
-                </th>
-                <th className="w-[8%] px-4 sm:px-4 md:px-4 lg:px-0">V DE gE</th>
-                <th className="w-[8%] px-4 sm:px-4 md:px-4 lg:px-0">
-                  V NDE mm/s
-                </th>
-                <th className="w-[8%] px-4 sm:px-4 md:px-4 lg:px-0">
-                  V NDE gE
-                </th>
-                <th className="w-[8%] px-4  sm:px-4 md:px-4 lg:px-0">
-                  T &deg; C
-                </th>
-                <th className="w-[10%] px-4  sm:px-4 md:px-4 lg:px-0">
-                  Anomali
-                </th>
+                <th className="px-2">Grinder</th>
+                <th className="px-4 sm:px-4 md:px-4"> Vib &#8597; DE mm/s</th>
+                <th className="px-4 sm:px-4 md:px-4">Vib &#8597; DE gE</th>
+                <th className="">Vib &harr; DE mm/s</th>
+                <th className=" ">Vib &harr; DE gE</th>
+                <th className=" ">Vib &#8597; NDE mm/s</th>
+                <th className=" ">Vib &#8597; NDE gE</th>
+                <th className=" ">Vib &harr; NDE mm/s</th>
+                <th className=" ">Vib &harr; NDE gE</th>
+                <th className=" ">Temp</th>
+                <th className=" ">Arus R</th>
+                <th className=" ">Arus S</th>
+                <th className=" ">Arus T</th>
+                <th className="">Keterangan</th>
               </tr>
             </thead>
             <tbody>
-              <tr className="border-separate h-10">
-                <td className="">1</td>
-                <td className="">Fan Turret Unwind Fan Turret Unwind</td>
-                <td className="">
-                  <select>
-                    <option>Halus</option>
-                    <option>Kasar</option>
-                  </select>
+              <tr className=" h-10">
+                <td className="sticky left-0 max-w-[40px] min-w-[40px] bg-white">
+                  1
                 </td>
+                <td className="">Transfer Waste Grinder</td>
                 <td className=" ">
                   <input className="w-full border" type="number"></input>
                 </td>
@@ -83,6 +72,24 @@ function ChecklistCT() {
                   <input className="w-full border" type="number"></input>
                 </td>
 
+                <td className=" ">
+                  <input className="w-full border" type="number"></input>
+                </td>
+                <td className=" ">
+                  <input className="w-full border" type="number"></input>
+                </td>
+                <td className=" ">
+                  <input className="w-full border" type="number"></input>
+                </td>
+                <td className=" ">
+                  <input className="w-full border" type="number"></input>
+                </td>
+                <td className=" ">
+                  <input className="w-full border" type="number"></input>
+                </td>
+                <td className=" ">
+                  <input className="w-full border" type="number"></input>
+                </td>
                 <td className=" ">
                   <input className="w-full border" type="number"></input>
                 </td>
@@ -95,11 +102,11 @@ function ChecklistCT() {
           </table>
         </div>
         <div className="flex flex-col basis-[5%] h-full bg-slate-1004">
-          <div className="flex flex-row justify-center pt-3 py-3">
-            <button className=" w-20 text-white bg-[#173D6E] hover:bg-[#9BB6D5] font-medium rounded-md text-sm px-5 py-2.5 mr-2">
+          <div className="flex flex-row justify-center pt-3 py-3 text-base sm:text-lg md:text-xl">
+            <button className=" w-fit text-white bg-[#173D6E] hover:bg-[#9BB6D5] font-medium rounded-md px-5 py-2.5 mr-2">
               Reset
             </button>
-            <button className=" w-20 text-white bg-[#173D6E] hover:bg-[#9BB6D5] font-medium rounded-md text-sm px-5 py-2.5 ml-2">
+            <button className=" w-fit text-white bg-[#173D6E] hover:bg-[#9BB6D5] font-medium rounded-md px-5 py-2.5 ml-2">
               Submit
             </button>
           </div>
